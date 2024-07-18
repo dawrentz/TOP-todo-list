d3e2c537fa816fdc3e2948bd1b225aa470c85f98
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-07-18 16:17:10 -0500

    Create initial barebones files. Set up npm/webpack for dev.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TOP-todo-list
 https://www.theodinproject.com/lessons/node-path-javascript-todo-list
+
+Create todo list app using modules while ultilizing npm/webpack. 
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    devtool: "inline-source-map", 
+    devServer: {
+        static: "./dist", 
+        hot: false,
+        // liveReload: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    optimization: {
+        // runtimeChunk: "single",
+    },
+};
+
+//and "look out for option"?
+//change multiple entries with more js modules
