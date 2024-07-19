a494f87fe03a23d070e50c4cf5a3124c39d231ad
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-07-19 15:30:52 -0500

    Create default todo. Store inputs in tasks obj.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-//create default todo list module
-
 //imports
 import "./reset.css";
 import "./style.css";
 import defaultTodo from "./defaultTodo";
 
 //declarations
+const tasks = [];
+
+
+//classes
+class Task {
+    constructor(title, project, descript, dueDate, priority, notes, checkList) {
+        this.title = title;
+        this.project = project;
+        this.descript = descript;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checkList = checkList;
+    }
+}
+
+function addTask(task) {
+    console.log(task);
+    tasks.push(task);
+    console.table(tasks);
+}
+
+//DOM cache
 const contentElement = document.querySelector("#content");
 
-defaultTodo(contentElement);
+//initialize
+defaultTodo(contentElement, Task, addTask);
 
 
+// console.log(tasks);
+// const test = new Task(1,2,3,4,5,6,7);
+// console.log(test);
+// test.addTask();
 
-
+// console.log(tasks);
+// console.log(tasks[0].title);
